e276f39e7b8063fcb3220b2645afc65c1b2bac54
daria <szaszdaria2004@gmail.com>
2024-10-15 16:03:43 +0300

    metoda2 care gaseste cel mai scump obiect

--- src/Aufgabe4.java
+++ src/Aufgabe4.java
@@ -10,4 +10,22 @@ public class Aufgabe4 {
         }
         return minPrice;
     }
+
+    // 2.Metoda pt a gasi cel mai scump obiect(tastatura sau USB)
+    public int expensive_obiect(int[] keyboards, int[] usbDrives) {
+        int maxPrice = keyboards[0];// Presupunem ca prima tastatura este cea mai scumpa
+        // Parcurgem lista de tastaturi pt a gasi cea mai scumpa
+        for (int price : keyboards) {
+            if (price > maxPrice) {
+                maxPrice = price; // Actualizam daca gasim o tastatura mai scumpă
+            }
+        }
+        // Parcurgem lista de USB-uri pt a vedea daca este unul mai scump decat cea mai scumpa tastatura gasita
+        for (int price : usbDrives) {
+            if (price > maxPrice) {
+                maxPrice = price; // Actualizam dacă gasim un USB mai scump
+            }
+        }
+        return maxPrice;
+    }
 }
